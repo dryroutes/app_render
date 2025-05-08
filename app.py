2b11d1ff255eb498fcbda74764484b02e640b913
dryroutes <proyii414@gmail.com>
2025-05-08 22:59:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -21,3 +21,5 @@ def ruta():
         return jsonify({"ruta": coords})
     except Exception as e:
         return jsonify({"error": str(e)}), 400
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
